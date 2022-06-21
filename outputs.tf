b60a9475a9c6ef92df83200feccfec0ba1ad26cc
kral2 <cetin@hashicorp.com>
2022-06-21 16:38:56 +0200

    feat: add auto-updating all_attributes outputs
    update examples to show how to use module outputs

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,8 @@ output "hcp_hvn_cidr_block" {
   description = "Hashicorp Cloud Platform CIDR block"
   value       = hcp_hvn.network.cidr_block
 }
+
+output "hcp_hvn_all_attributes" {
+  description = "all attributes of the created HVN"
+  value = { for k, v in hcp_hvn.network : k => v }
+}
